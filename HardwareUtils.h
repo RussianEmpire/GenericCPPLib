6f42812d0135665aef72694ddd77ade109fc5bcd
Eugen <dev3@exo-group.com>
2015-12-14 06:44:24 +0300

    'HardwareUtils' module fixed for GCC compilation

--- HardwareUtils.h
+++ HardwareUtils.h
@@ -78,6 +78,9 @@ private:
 
 };
 
-const CPUInfo CPUInfo::INSTANCE;
+#ifdef _MSC_VER
+  // GCC might NOT build with this, while MS VS 2013 will NOT build withOUT this
+  const CPUInfo CPUInfo::INSTANCE;
+#endif
 
 #endif // HardwareUtilsH
