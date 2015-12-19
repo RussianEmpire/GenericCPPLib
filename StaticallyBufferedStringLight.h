b77ede8ef672e5b637552a7a2e50bd08bd2f8da3
Eugen <dev3@exo-group.com>
2015-12-19 08:43:47 +0300

    'HashCodeChecker' class removed from 'StaticallyBufferedStringLight' module

--- StaticallyBufferedStringLight.h
+++ StaticallyBufferedStringLight.h
@@ -7,6 +7,7 @@
 #include "MathUtils.h"
 #include "AllocatorInterface.h"
 #include "GenericRAIterator.h"
+#include "HashCodeChecker.h"
 
 EXEC_MEMBER_FUNC_IF_PRESENT(getHashIfKnown, size_t())
 EXEC_MEMBER_FUNC_IF_PRESENT(hashAlgoID, size_t())
@@ -495,12 +496,12 @@ public:
 
   // Returns the first elem. of the string
   TElemType& front() throw() {
-    return at(0U);
+    return at(size_t());
   }
   
   // Returns the first elem. of the string
   const TElemType& front() const throw() {
-    return at(0U);
+    return at(size_t());
   }
 
   // Appends character 'c' to the end of the string, increasing its length by one
@@ -762,88 +763,5 @@ namespace std {
 };
 
 typedef StaticallyBufferedStringLight<> StrLight;
-
-// Assister class, static singleton (C++11 synched)
-// Does some work at the program launch
-class HashCodeChecker {
-
-public:
-
-  // Static init. is a thread safe in C++11
-  static const HashCodeChecker INSTANCE;
-
-  const bool hashOfLargerStrLarger = false;
-
-private:
-
-  // Will be launched at the very beginning of the program execution
-  HashCodeChecker() throw()
-    : hashOfLargerStrLarger(ishashOfLargerStrLarger()) {}
-
-  ~HashCodeChecker() = default;
-
-  HashCodeChecker(const HashCodeChecker&) throw() = delete;
-  HashCodeChecker(HashCodeChecker&&) throw() = delete;
-  HashCodeChecker& operator=(const HashCodeChecker&) throw() = delete;
-  HashCodeChecker& operator=(HashCodeChecker&&) throw() = delete;
-
-  struct HashChecker {
-    size_t operator()(const std::string& str) const throw() {
-      if (!COLLECT_STATS && !NEXT_HASH_LARGER_OR_EQ) return 0U; // skip [OPTIMIZATION]
-      STR = str;
-
-      const auto currHash = STR.hash();
-      const auto nextLargerOrEq = (currHash >= PREV_HASH);
-      nextLargerOrEq ? ++STATS[1U] : ++*STATS;
-      NEXT_HASH_LARGER_OR_EQ &= nextLargerOrEq;
-      PREV_HASH = currHash;
-
-      return currHash;
-    }
-
-    static bool COLLECT_STATS; // will be initially false as a static
-    // First - next lower count (NOT OK!), second - next larger count (OK); both zero at the start
-    static size_t STATS[2U];
-    static size_t PREV_HASH; // will be initially zero as a static
-    static bool NEXT_HASH_LARGER_OR_EQ; // initially true!!
-    static StaticallyBufferedStringLight<char, 7U> STR;
-  };
-
-  template <const bool SkipStatistics = true>
-  static bool ishashOfLargerStrLarger() throw() {
-    //// Test A
-    static const auto MAX_LEN_ = 55U;
-    static_assert('A' + MAX_LEN_ < 128, "Incorrect max. len.");
-    StaticallyBufferedStringLight<char, MAX_LEN_> str;
-
-    decltype(str.hash()) prevHash = str.hash(), nowHash = 0U;
-    auto currChar = 'A'; // from 'A' to 'A'+MAX_LEN_
-    auto nextHashLargerOrEqCount = 0U, nextHashLowerCount = 0U;
-
-    for (auto charIdx = 0U; charIdx < str.max_size(); ++charIdx) {
-      str += currChar;
-      ++currChar;
-      nowHash = str.hash();
-
-      nowHash >= prevHash ? ++nextHashLargerOrEqCount : ++nextHashLowerCount;
-      if (SkipStatistics && nextHashLowerCount) return false;
-      prevHash = nowHash;
-    }
-    //// Test B
-    char strBuf[4U] = {0};
-    HashTester::Params<std::extent<decltype(strBuf)>::value> params(strBuf);
-    const auto result = HashTester::createAndTestCharSeq<HashChecker>(params, false);
-
-    return !nextHashLowerCount && result && HashChecker::NEXT_HASH_LARGER_OR_EQ;
-  }
-};
-
-const HashCodeChecker HashCodeChecker::INSTANCE;
-
-bool HashCodeChecker::HashChecker::COLLECT_STATS;
-size_t HashCodeChecker::HashChecker::STATS[2U];
-size_t HashCodeChecker::HashChecker::PREV_HASH;
-bool HashCodeChecker::HashChecker::NEXT_HASH_LARGER_OR_EQ = true;
-StaticallyBufferedStringLight<char, 7U> HashCodeChecker::HashChecker::STR;
 
 #endif // StaticallyBufferedStringLightH
