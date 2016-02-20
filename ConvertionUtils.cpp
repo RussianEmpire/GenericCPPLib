a3c623c6becc65cd3db4b45ad8fc26fb173f893e
Eugen <dev3@exo-group.com>
2016-02-20 05:15:18 +0300

    'ConvertionUtils' module (with tests) added

---
+++ ConvertionUtils.cpp
@@ -0,0 +1,6 @@
+﻿#include "ConvertionUtils.h"
+
+const char* const ConvertionUtils::ENG_GB_VERBAL_DELIMITER = "and";
+const char* const ConvertionUtils::DEFAULT_DELIMITER = " ";
+
+const ConvertionUtils::LocaleSettings ConvertionUtils::LocaleSettings::DEFAULT_LOCALE_SETTINGS;
