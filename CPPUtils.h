6cb67156f08140189aae5965c008a89e770ef4f0
Eugen <dev3@exo-group.com>
2016-06-20 17:45:15 +0400

    'CPPUtils' module added

---
+++ CPPUtils.h
@@ -0,0 +1,12 @@
+﻿#ifndef CPPUtilsH
+#define CPPUtilsH
+
+#ifndef CONSTEXPR_ // used to conditionally enable 'constexpr' funcs & variables
+  #if __cplusplus >= 201103L // http://stackoverflow.com/questions/5047971/how-do-i-check-for-c11-support
+    #define CONSTEXPR_ constexpr
+  #else
+    #define CONSTEXPR_
+  #endif
+#endif // CONSTEXPR_
+
+#endif // CPPUtilsH
