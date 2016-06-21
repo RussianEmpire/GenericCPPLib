8e21b4294496dfda50bb83df612e13166c7c18e2
Eugen <dev3@exo-group.com>
2016-06-21 23:03:00 +0400

    'TestUtils' module updated

--- SRC/TestUtils.h
+++ SRC/TestUtils.h
@@ -1,6 +1,8 @@
 ﻿#ifndef TestUtilsH
 #define TestUtilsH
 
+//// [!] Version 1.001 [!]
+
 #include "CPPUtils.h"   // for 'CONSTEXPR_'
 #include "MacroUtils.h" // for 'MAKE_STR_'
 
@@ -8,23 +10,33 @@
 #include <stdexcept> // for 'std::runtime_error'
 
 namespace TestUtils {
-  // 'CustomErrMsg' CAN be empty ("") - if so, auto. generated one will be used instead
-  #define ASSERT_(Assumption, CustomErrMsg)\
-  if (const volatile auto result = !(Assumption)) {/**/\
-    assert(! #Assumption);\
-    \
-    static CONSTEXPR_ const char* const CUSTOM_ERR_MSG_ = "" CustomErrMsg;\
-    static CONSTEXPR_ auto const AUTO_ERR_MSG_ =\
-      "[!] '"#Assumption"' failed at line #" MAKE_STR_(__LINE__) " in file \"" __FILE__ "\"";\
-    static CONSTEXPR_ auto const ERR_MSG_ = /* if custom is NOT empty - use it*/\
-      CUSTOM_ERR_MSG_ && *CUSTOM_ERR_MSG_ ? CUSTOM_ERR_MSG_ : AUTO_ERR_MSG_;\
-    \
-    std::cout << '\n' << ERR_MSG_ << '\n';\
-    throw std::runtime_error(ERR_MSG_);\
-  }
-
-  // Uses auto. generated err. msg.
-  #define ASSERT__(Assumption) ASSERT_(Assumption, "")
+  #ifndef ASSERT_
+    // 'CustomErrMsg' CAN be empty ("") - if so, auto. generated one will be used instead
+    #define ASSERT_(Assumption, CustomErrMsg)\
+    /*'volatile' used to prevent the compiler from optimizing out the code
+       (to ensure it will be executed AND tested then)*/\
+    if (const volatile auto assert__result__local__temp__ = !(Assumption)) {\
+      assert(! #Assumption);\
+      \
+      static CONSTEXPR_ const char* const CUSTOM_ERR_MSG_ = "" CustomErrMsg;\
+      static CONSTEXPR_ auto const AUTO_ERR_MSG_ =\
+        "[!] '"#Assumption"' failed at line #" MAKE_STR_(__LINE__) " in file \"" __FILE__ "\"";\
+      static CONSTEXPR_ auto const ERR_MSG_ = /* if custom is NOT empty - use it*/\
+        CUSTOM_ERR_MSG_ && *CUSTOM_ERR_MSG_ ? CUSTOM_ERR_MSG_ : AUTO_ERR_MSG_;\
+      \
+      std::cout << '\n' << ERR_MSG_ << '\n';\
+      throw std::runtime_error(ERR_MSG_);\
+    }
+  #else
+    static_assert(false, "Error: 'ASSERT_' macro is already defined somewhere");
+  #endif
+  
+  #ifndef ASSERT__
+    // Uses auto. generated err. msg.
+    #define ASSERT__(Assumption) ASSERT_(Assumption, "")
+  #else
+    static_assert(false, "Error: 'ASSERT__' macro is already defined somewhere");
+  #endif
 }
 
 #endif // TestUtilsH
