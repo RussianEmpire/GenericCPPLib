0a9665320951fc485c1e4db5b3f63863620698f8
Eugen <dev3@exo-group.com>
2016-06-21 23:31:03 +0400

    'CPPUtils' module updated

--- SRC/CPPUtils.h
+++ SRC/CPPUtils.h
@@ -1,12 +1,16 @@
 ﻿#ifndef CPPUtilsH
 #define CPPUtilsH
 
+//// [!] Version 1.001 [!]
+
 #ifndef CONSTEXPR_ // used to conditionally enable 'constexpr' funcs & variables
   #if __cplusplus >= 201103L // http://stackoverflow.com/questions/5047971/how-do-i-check-for-c11-support
     #define CONSTEXPR_ constexpr
   #else
     #define CONSTEXPR_
   #endif
+#else
+  static_assert(false, "Error: 'CONSTEXPR_' macro is already defined somewhere");
 #endif // CONSTEXPR_
 
 #endif // CPPUtilsH
