e5095daac1b58c0805a7296fb36f5ec13a1d6891
Eugen <dev3@exo-group.com>
2016-06-23 14:33:15 +0400

    'MathAndBitUtils' module updated

--- SRC/MathAndBitUtils.cpp
+++ SRC/MathAndBitUtils.cpp
@@ -3,6 +3,6 @@
 const long double MathUtils::DEFAULT_CMP_EPSILON_NEIGHBORHOOD_ = 1e-9L;
 const MathUtils::ByteOrderTester MathUtils::ByteOrderTester::INSTANCE;
 
-//// [!] Beware of the static initialization order fiasco ('BitPackageOrder' uses 'ByteOrderTesterEx') [!]
+//// [!] Beware of the static initialization order fiasco ('BitPackageOrderTester' uses 'ByteOrderTesterEx') [!]
 const BitUtils::ByteOrderTesterEx BitUtils::ByteOrderTesterEx::INSTANCE;
-const BitUtils::BitPackageOrder BitUtils::BitPackageOrder::INSTANCE;
+const BitUtils::BitPackageOrderTester BitUtils::BitPackageOrderTester::INSTANCE;
